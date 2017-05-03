2744ab284b3805c9ad19334b8d1b2c9b4cab6d1e
badelie <badelie@msn.com>
2017-05-03 14:15:42 +0100

    Update config.sh
    Updated to latest v3 module template

--- config.sh
+++ config.sh
@@ -10,18 +10,12 @@
 # 
 # Instructions:
 # 
-# 1. Place your files into system folder
-# 2. Fill in all sections in this file
-# 3. For advanced features, add commands into the script files under common:
-#    post-fs.sh, post-fs-data.sh, service.sh
-# 4. Change the "module.prop" under common with the info of your module
-# 
-##########################################################################################
-##########################################################################################
-# 
-# Limitations:
-# 1. Can not place any new items under /system root!! e.g. /system/newfile, /system/newdir
-#    Magisk will delete these items at boot.
+# 1. Place your files into system folder (delete the placeholder file)
+# 2. Fill in your module's info into module.prop
+# 3. Configure the settings in this file (common/config.sh)
+# 4. For advanced features, add shell commands into the script files under common:
+#    post-fs-data.sh, service.sh
+# 5. For changing props, add your additional/modified props into common/system.prop
 # 
 ##########################################################################################
 
@@ -31,24 +25,21 @@
 
 # NOTE: This part has to be adjusted to fit your own needs
 
-# Is this a cache mod?
-CACHEMOD=false
-
-# This will be the folder name under /magisk or /cache/magisk
+# This will be the folder name under /magisk
 # This should also be the same as the id in your module.prop to prevent confusion
 MODID=EmojiOne-Samsung
 
-# Set to true if you need automount
+# Set to true if you need to enable Magis Mount
 # Most mods would like it to be enabled
 AUTOMOUNT=true
 
-# Set to true if you need post-fs script (Only available in cache mods)
+# Set to true if you need to load system.prop
 POSTFS=false
 
-# Set to true if you need post-fs-data script (Only available in non-cache mods)
+# Set to true if you need post-fs-data script
 POSTFSDATA=false
 
-# Set to true if you need late_start service script (Only available in non-cache mods)
+# Set to true if you need late_start service script
 LATESTARTSERVICE=false
 
 ##########################################################################################
@@ -71,7 +62,8 @@ print_modname() {
 # By default Magisk will merge your files with the original system
 # Directories listed here however, will be directly mounted to the correspond directory in the system
 
-# Construct your own list
+# Construct your own list here, it will overwrite the example
+# !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
 REPLACE="
 "
 
